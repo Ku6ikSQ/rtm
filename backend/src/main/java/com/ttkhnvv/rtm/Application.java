32ac674f34a5b514c91365972d5fe447a23ddd8b
Timofey Tikhonov <tikhonow66@gmail.com>
2026-04-23 16:22:41 +0500

    chore(config): spring boot added

--- backend/src/main/java/com/ttkhnvv/rtm/Application.java
+++
@@ -1,8 +0,0 @@
-package src.main.java.com.ttkhnvv.rtm;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
-
---
+++ backend/src/test/java/com/ttkhnvv/rtm/RtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttkhnvv.rtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
