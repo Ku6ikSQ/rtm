cb64abdca0f4f5b716cfdd846e8594fd0fb74407
Timofey Tikhonov <tikhonow66@gmail.com>
2026-04-27 18:07:38 +0500

    chore(backend, config): postgresql added

--- backend/src/main/java/com/ttkhnvv/rtm/RtmApplication.java
+++ backend/src/main/java/com/ttkhnvv/rtm/RtmApplication.java
@@ -3,20 +3,12 @@ package com.ttkhnvv.rtm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
 @SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
 		FlywayAutoConfiguration.class,
 })
 public class RtmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RtmApplication.class, args);
 	}
-
 }
