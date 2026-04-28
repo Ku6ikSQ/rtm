f9d32c555a8799f43dda960892f4129cfceeeeb7
Timofey Tikhonov <tikhonow66@gmail.com>
2026-04-28 19:13:21 +0500

    feat(backend, security): user details service were implemented

---
+++ backend/src/main/java/com/ttkhnvv/rtm/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ttkhnvv.rtm.repository;
+
+import com.ttkhnvv.rtm.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findUserByUsername(String username);
+}
---
+++ backend/src/main/java/com/ttkhnvv/rtm/security/UserPrincipal.java
@@ -0,0 +1,31 @@
+package com.ttkhnvv.rtm.security;
+
+import com.ttkhnvv.rtm.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
