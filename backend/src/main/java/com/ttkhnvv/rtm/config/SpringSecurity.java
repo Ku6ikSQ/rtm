248a67b5362c4f5e8992e51cc31f75ad51c74947
Timofey Tikhonov <tikhonow66@gmail.com>
2026-04-28 11:58:24 +0500

    chore(backend, security): added initial spring security configuration

---
+++ backend/src/main/java/com/ttkhnvv/rtm/config/SpringSecurity.java
@@ -0,0 +1,29 @@
+package com.ttkhnvv.rtm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurity {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return null;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/**").permitAll())
+                .build();
+    }
+}
