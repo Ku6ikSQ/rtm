61a77cd249a6a1decca6e805e4d6cc56f1f958f5
Timofey Tikhonov <tikhonow66@gmail.com>
2026-05-02 19:48:59 +0500

    test(backend, service): remove unused import in JwtServiceTest

--- backend/src/test/java/com/ttkhnvv/rtm/security/jwt/JwtServiceTest.java
+++ backend/src/test/java/com/ttkhnvv/rtm/security/jwt/JwtServiceTest.java
@@ -6,7 +6,6 @@ import com.ttkhnvv.rtm.entity.user.UserRole;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
