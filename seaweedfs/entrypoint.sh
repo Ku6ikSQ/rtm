854c53f4d564a35f515c56152b0e63e926b4b02c
Timofey Tikhonov <tikhonow66@gmail.com>
2026-05-13 18:11:35 +0500

    feat(config): added seaweed init script

---
+++ seaweedfs/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+cat > /tmp/s3.conf << EOF
+{
+  "signingKey": "seaweedfs-sts-key",
+  "identities": [
+    {
+      "name": "admin",
+      "credentials": [
+        {
+          "accessKey": "$STORAGE_ACCESS_KEY",
+          "secretKey": "$STORAGE_SECRET_KEY"
+        }
+      ],
+      "actions": ["Admin", "Read", "Write", "List", "Tagging"]
+    }
+  ]
+}
+EOF
+
+exec weed server -dir=/data -s3 -s3.port=8333 -s3.config=/tmp/s3.conf -filer -filer.port=8888
